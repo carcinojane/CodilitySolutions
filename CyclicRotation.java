625eaf8e5258cbd8b6527be6fd27d9e73dce9bfc
Jane Lee <e0533359@u.nus.edu>
2020-12-10 17:30:26 +0800

    Added pseudo code

--- CyclicRotation.java
+++ CyclicRotation.java
@@ -1,21 +1,28 @@
 import java.util.*;
+//find question at https://app.codility.com/programmers/lessons/2-arrays/
 
 class CyclicRotation {
     public int[] solution(int[] A, int K) {
-        // write your code in Java SE 8
+        // initialize variables
         Integer maxLength, maxIndex;
-
         maxLength = A.length;
         maxIndex = maxLength - 1;
+
+        // Create empty array for re-arranged int[]
         int[] newArr = new int[maxLength];
 
-        if (K == maxLength || maxLength == 0 || maxIndex == 0 || K % maxLength == 0)
+        if (maxLength == 0 || maxIndex == 0 || // Filter out (1)Array contains less than 2,
+                K == maxLength || K % maxLength == 0 // (2)Rotation results in the same order,
+                || K == 0) // K=0
             return A;
 
+        // get number of necessary rotations
         Integer Kr = K > maxLength ? K % maxLength : K;
 
         for (int i = 0; i < maxLength; i++) {
+            // get new index of items
             Integer newIndex = i + Kr > maxIndex ? i + Kr - maxIndex - 1 : i + Kr;
+            // add to new array
             newArr[newIndex] = A[i];
         }
         return newArr;
